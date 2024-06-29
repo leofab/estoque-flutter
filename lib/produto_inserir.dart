403ddf79eaa03758deee72de02c5f6500a836219
leofab <leofabr3@gmail.com>
2024-06-29 19:24:21 -0300

    dropDownMenu implemented

--- lib/produto_inserir.dart
+++ lib/produto_inserir.dart
@@ -7,6 +7,34 @@ class ProdutoInserir extends StatefulWidget {
   State<ProdutoInserir> createState() => _ProdutoInserirState();
 }
 
+enum Tipos {
+  absinto("Absinto", Colors.green),
+  amarguinha("Amarguinha", Colors.green),
+  cachaca("Cachaça", Colors.green),
+  cerveja("Cerveja", Colors.green),
+  champanhe("Champanhe", Colors.green),
+  conhaque("Conhaque", Colors.green),
+  gim("Gim", Colors.green),
+  grappa("Grappa", Colors.green),
+  hidromel("Hidromel", Colors.green),
+  licor("Licor", Colors.green),
+  rum("Rum", Colors.green),
+  saque("Saque", Colors.green),
+  sidra("Sidra", Colors.green),
+  tequila("Tequila", Colors.green),
+  vermute("Vermute", Colors.green),
+  vinho("Vinho", Colors.green),
+  vodka("Vodka", Colors.green),
+  whisky("Whisky", Colors.green),
+  semAlcool("Sem Álcool", Colors.green),
+  gelo("Gelo", Colors.green),
+  outros("Outros", Colors.green);
+
+  const Tipos(this.tipo, this.cor);
+  final String tipo;
+  final Color cor;
+}
+
 class _ProdutoInserirState extends State<ProdutoInserir> {
   final key = GlobalKey<FormState>();
   final myControllerNome = TextEditingController();
@@ -22,6 +50,24 @@ class _ProdutoInserirState extends State<ProdutoInserir> {
   final myControllerLote = TextEditingController();
   final myControllerLocal = TextEditingController();
   final myControllerObservacao = TextEditingController();
+
+  @override
+  void dispose() {
+    myControllerNome.dispose();
+    myControllerTipo.dispose();
+    myControllerDescricao.dispose();
+    myControllerPreco.dispose();
+    myControllerQuantidade.dispose();
+    myControllerUnidade.dispose();
+    myControllerCategoria.dispose();
+    myControllerFornecedor.dispose();
+    myControllerData.dispose();
+    myControllerValidade.dispose();
+    myControllerLote.dispose();
+    myControllerLocal.dispose();
+    myControllerObservacao.dispose();
+    super.dispose();
+  }
 
   void alerta(BuildContext context) {
     showDialog(
@@ -57,18 +103,34 @@ class _ProdutoInserirState extends State<ProdutoInserir> {
             child: Form(
               key: key,
               child: Column(
-                children: [
+                children: <Widget>[
                   TextFormField(
                     decoration: const InputDecoration(labelText: "Nome"),
                     controller: myControllerNome,
+                    validator: (value) =>
+                        value!.isEmpty ? "Campo obrigatório" : null,
                   ),
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: "Tipo"),
+                  DropdownMenu<Tipos>(
+                    initialSelection: Tipos.absinto,
                     controller: myControllerTipo,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: "Descrição"),
-                    controller: myControllerDescricao,
+                    requestFocusOnTap: true,
+                    label: const Text("Tipo"),
+                    onSelected: (Tipos? cor) {
+                      setState(() {
+                        myControllerTipo.text = cor!.tipo;
+                      });
+                    },
+                    dropdownMenuEntries: Tipos.values
+                        .map<DropdownMenuEntry<Tipos>>((Tipos tipo) {
+                      return DropdownMenuEntry<Tipos>(
+                        value: tipo,
+                        label: tipo.name,
+                        enabled: tipo.name != 'Grey',
+                        style: MenuItemButton.styleFrom(
+                          foregroundColor: tipo.cor,
+                        ),
+                      );
+                    }).toList(),
                   ),
                   TextFormField(
                     decoration: const InputDecoration(labelText: "Preço"),
