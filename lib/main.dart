9163437526890e7a44b498582ee3737ece15111a
leofab <leofabr3@gmail.com>
2024-07-05 14:24:34 -0300

    create ProdutosProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'screens/login.dart';
 import 'screens/produtos_lista.dart';
 import 'screens/produto_vendas.dart';
+import 'providers/produtos_provider.dart';
 
 Future main() async {
   runApp(const MainApp());
@@ -14,10 +15,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Estoque App', initialRoute: '/login', routes: {
-      '/login': (context) => const LoginScreen(),
-      '/produtos': (context) => const ProdutosLista(),
-      '/vendas': (context) => const ProdutoVendas(),
-    });
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ProdutosProvider()),
+      ],
+      child: MaterialApp(title: 'Estoque App', initialRoute: '/login', routes: {
+        '/login': (context) => const LoginScreen(),
+        '/produtos': (context) => const ProdutosLista(),
+        '/vendas': (context) => const ProdutoVendas(),
+      }),
+    );
   }
 }
---
+++ lib/providers/produtos_provider.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/produto.dart';
+
+class ProdutosProvider extends ChangeNotifier {
+  List<Produto> _produtos = [
+    Produto(
+      id: 1,
+      tipo: Tipos.vinho.name,
+      nome: 'Vinho Sao Francisco',
+      valorCompraTotal: 50.00,
+      preco: 10.00,
+      quantidade: 10,
+      unidade: 'L',
+    ),
+    Produto(
+      id: 2,
+      tipo: Tipos.cachaca.name,
+      nome: 'Cachaça Ypioca',
+      valorCompraTotal: 25.00,
+      preco: 5.00,
+      quantidade: 5,
+      unidade: 'L',
+    ),
+    Produto(
+      id: 3,
+      tipo: Tipos.vodka.name,
+      nome: 'Vodka Smirnoff',
+      valorCompraTotal: 75.00,
+      preco: 15.00,
+      quantidade: 15,
+      unidade: 'L',
+    ),
+    Produto(
+      id: 4,
+      tipo: Tipos.whisky.name,
+      nome: 'Whisky Johnnie Walker',
+      valorCompraTotal: 100.00,
+      preco: 20.00,
+      quantidade: 20,
+      unidade: 'L',
+    ),
+    Produto(
+      id: 5,
+      tipo: Tipos.cerveja.name,
+      nome: 'Cerveja Skol',
+      valorCompraTotal: 10.00,
+      preco: 2.00,
+      quantidade: 20,
+      unidade: 'L',
+    ),
+    Produto(
+      id: 6,
+      tipo: Tipos.gelo.name,
+      nome: 'Gelo',
+      valorCompraTotal: 1.00,
+      preco: 1.00,
+      quantidade: 5,
+      unidade: 'kg',
+    ),
+    Produto(
+      id: 7,
+      tipo: Tipos.outros.name,
+      nome: 'Amendoin',
+      valorCompraTotal: 7.00,
+      preco: 7.00,
+      quantidade: 7,
+      unidade: 'kg',
+    ),
+    Produto(
+      id: 8,
+      tipo: Tipos.rum.name,
+      nome: 'Rum Motilla',
+      valorCompraTotal: 8.00,
+      preco: 8.00,
+      quantidade: 8,
+      unidade: 'L',
+    ),
+    Produto(
+      id: 9,
+      tipo: Tipos.semAlcool.name,
+      nome: 'Refrigerante',
+      valorCompraTotal: 3.00,
+      preco: 3.00,
+      quantidade: 3,
+      unidade: 'L',
+    ),
+    Produto(
+      id: 10,
+      tipo: Tipos.vinho.name,
+      nome: 'Vinho Aurora',
+      valorCompraTotal: 4.00,
+      preco: 4.00,
+      quantidade: 4,
+      unidade: 'L',
+    ),
+    Produto(
+      id: 11,
+      tipo: Tipos.tequila.name,
+      nome: 'Tequila Jose Cuervo',
+      valorCompraTotal: 5.00,
+      preco: 5.00,
+      quantidade: 5,
+      unidade: 'L',
+    ),
+    Produto(
+      id: 12,
+      tipo: Tipos.absinto.name,
+      nome: 'Absinto',
+      valorCompraTotal: 6.00,
+      preco: 6.00,
+      quantidade: 6,
+      unidade: 'L',
+    ),
+    Produto(
+      id: 13,
+      tipo: Tipos.cerveja.name,
+      nome: 'Cerveja Heineken',
+      valorCompraTotal: 7.00,
+      preco: 7.00,
+      quantidade: 7,
+      unidade: 'L',
+    ),
+    Produto(
+      id: 14,
+      tipo: Tipos.cerveja.name,
+      nome: 'Cerveja Budweiser',
+      valorCompraTotal: 8.00,
+      preco: 8.00,
+      quantidade: 8,
+      unidade: 'L',
+    ),
+  ];
+
+  List<Produto> get produtos {
+    return [..._produtos];
+  }
+
+  void adicionarProduto(Produto produto) {
+    _produtos.add(produto);
+    notifyListeners();
+  }
+
+  void atualizarProduto(String id, Produto novoProduto) {
+    final produtoIndex = _produtos.indexWhere((produto) => produto.id == id);
+    if (produtoIndex >= 0) {
+      _produtos[produtoIndex] = novoProduto;
+      notifyListeners();
+    } else {
+      print('...');
+    }
+  }
+
+  void deletarProduto(String id) {
+    _produtos.removeWhere((produto) => produto.id == id);
+    notifyListeners();
+  }
+
+  Produto encontrarPorId(String id) {
+    return _produtos.firstWhere((produto) => produto.id == id);
+  }
+
+  List<Produto> filtrarPorNome(String nome) {
+    return _produtos.where((produto) => produto.nome.contains(nome)).toList();
+  }
+
+  List<Produto> filtrarPorTipo(String tipo) {
+    return _produtos.where((produto) => produto.tipo.contains(tipo)).toList();
+  }
+
+  static ProdutosProvider of(BuildContext context, {bool listen = true}) {
+    return Provider.of<ProdutosProvider>(context, listen: listen);
+  }
+}
